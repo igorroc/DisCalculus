f4354b57d42892a61d7d3b62848ea8e69901102d
IgorRoc <igor_roc@hotmail.com.br>
2020-06-24 11:12:56 -0300

    Bugs fixed

--- commands/calculo.js
+++ commands/calculo.js
@@ -1,19 +1,30 @@
 const Discord = require("discord.js")
 
-
+const pi = '3.14159265359'
 module.exports.run = async (bot, message, args) => {
     console.log(`\n■▶ [LOGS] ⇥ Usuário "${message.author.username}" usou o comando Calculo`)
     
-    let conta = args.toString()
+    let conta = args.toString().replace(/pi|π/gm, pi).replace(/,/gm, "")
+
     if(!conta){
         return message.channel.send("Informe alguma conta para eu realizar\n> Para mais informações digite `+help calculo`")
     }
     if(conta.search(/[a-z]|[A-Z]/gm) > -1){
-        return message.channel.send("Ainda não posso fazer cálculo com variáveis")
+        return message.channel.send("Ainda não posso fazer cálculos avançados")
     }
 
 
-    //message.channel.send(args)
+    let resultado
+    try {
+        resultado = eval(conta)
+    } catch (error) {
+        console.log(`↳ Erro ao calcular: "${conta}"`)
+        return message.channel.send("Erro ao calcular")
+    }
+    
+    console.log(`↳ Conta: ${conta} | Resultado: ${resultado}`)
+
+    message.channel.send(`\`\`\`\n${conta} = ${resultado}\n\`\`\``)
 
 }
 
@@ -21,7 +32,7 @@ module.exports.run = async (bot, message, args) => {
 module.exports.config = {
     name: "calculo",
     description: "Realiza o calculo especificado!",
-    usage: "+calculo [(5+1)^2]",
+    usage: "+calculo [(5+1)*2]",
     accessableby: "Membros",
     aliases: ["calc", "c"]
 }
