e71b01eaa3a13188af2257de7ed9d364e457f688
IgorRoc <igor_roc@hotmail.com.br>
2020-06-24 13:07:19 -0300

    Log atualizado

--- commands/square.js
+++ commands/square.js
@@ -9,14 +9,17 @@ module.exports.run = async (bot, message, args) => {
     let valor = args.toString().replace(/pi|π/gm, pi).replace(/,/gm, "")
     
     if(!valor){
-        return message.channel.send("Informe algum valor para eu realizar a raiz\n> Para mais informações digite `+help square`")
+        console.log(`↳ ⚠️ Usuário não informou um valor `)
+        return message.channel.send("`❌` Informe algum valor para eu realizar a raiz\n> Para mais informações digite `+help square`")
     }
     if(valor.search(/[^0-9]/gm) > -1){
         if(valor.match(/[.]/g)||[].length > 1){
+            console.log(`↳ ⚠️ Usuário digitou mais de um "."`)
             return message.channel.send("Digite apenas um ` . `")
         }else if(valor.match(/[.]/gm)||[].length == 1){
             // ok apenas um "."
         }else{
+            console.log(`↳ ⚠️ Usuário digitou algum caractere`)
             return message.channel.send("Digite apenas números")
         }
     }
@@ -25,13 +28,12 @@ module.exports.run = async (bot, message, args) => {
     try {
         resultado = Math.sqrt(valor)
     } catch (error) {
-        console.log(`↳ Erro ao calcular: "${valor}"`)
-        return message.channel.send("Erro ao calcular")
+        console.log(`↳ ⚠️ Erro ao calcular raiz de "${valor}" `)
+        return message.channel.send("`❌` Erro ao calcular, tente novamente")
     }
 
-    console.log(`↳ Valor: ${valor} | Resultado: ${resultado}`)
-
     message.channel.send(`\`\`\`\n√(${valor}) = ${resultado}\n\`\`\``)
+    console.log(`↳ ✅ Operação finalizada!`)
 }
 
 
