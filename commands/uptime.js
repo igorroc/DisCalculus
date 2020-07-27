2aa83c0f7d9b40f184776de95f038de1eb839a2b
Igor Lima Rocha <igor_roc@hotmail.com.br>
2020-07-26 23:20:52 -0300

    [add]: comando para o tempo do bot no servidor

---
+++ commands/uptime.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+
+
+module.exports.run = async (bot, message, args) => {
+    const log = bot.guilds.cache.get('725691740538929225').channels.cache.get('725691977311453214')
+
+    console.log(`\n■▶ [LOGS] ⇥ Usuário "${message.author.username}" usou o comando "Uptime" no server "${message.guild.name}"`)
+    log.send(`\\▶️ [LOGS] ⇥ Usuário \` ${message.author.username} \` usou o comando \` Uptime \` no server \` ${message.guild.name} \`\n`)
+    
+    let up = ms(bot.uptime, {long: true})
+
+    const m = await message.channel.send(`I've been working for ${up} without accidents!`);
+    console.log(`↳ Uptime: ${up}`)
+
+}
+
+
+module.exports.config = {
+    name: "uptime",
+    description: "Shows up how long i've been online!",
+    usage: ".uptime",
+    accessableby: "Members",
+    aliases: ["up"]
+}
