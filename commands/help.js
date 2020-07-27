67ea9205a3f7c02dc836e29e7c9d4257d8ea0d65
Igor Lima Rocha <igor_roc@hotmail.com.br>
2020-07-26 23:38:07 -0300

    [fix]: correção de escrita

--- commands/help.js
+++ commands/help.js
@@ -54,7 +54,7 @@ module.exports.run = async (bot, message, args) => {
             }
         }
         Sembed.addField("\u200b", `For more information type \` ${prefix}help [command] \``)
-        .setFooter(`${bot.user.username} | Comands: ${bot.commands.size}`)
+        .setFooter(`${bot.user.username} | Commands: ${bot.commands.size}`)
         message.channel.send(Sembed)
     }
 
