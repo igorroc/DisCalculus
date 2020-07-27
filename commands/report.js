a44be1867149862698f673be4ff232c29b559e1a
Igor Lima Rocha <igor_roc@hotmail.com.br>
2020-07-26 23:41:47 -0300

    [fix]: blocos adicionados

--- commands/report.js
+++ commands/report.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
         var embed = new Discord.MessageEmbed()
             .setColor(colours.green_dark)
             .setAuthor(bot.user.username, message.guild.iconURL())
-            .setDescription(`\`\`\`md\n# Thank you for sending your report!\n\n# You can enter on the official server, if you want more help!\n\`\`\``)
+            .setDescription(`\`\`\`md\n# Thank you for sending your report!\n\`\`\`\n\`\`\`md\n# You can enter on the official server, if you want more help!\n\`\`\``)
             .addField("**Server Link:**", "https://discord.gg/QdqB5sR")
             .setFooter(`DisCalculus | Currently on ${bot.guilds.cache.size} servers!`)
 
@@ -27,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
         var embed = new Discord.MessageEmbed()
             .setColor(colours.red_light)
             .setAuthor(bot.user.username, message.guild.iconURL())
-            .setDescription(`\`\`\`md\n<\nType a message you want to send to my owner!\n>\n\nExample:\n<+report I'm having trouble with the calculation command. Can you help me?>\n\n<Disclaimer: By sending this message, my owner will be able to get your User Tag and then send you a direct message to help you!>\n\n# You can enter on the official server, if you want more help!\n\`\`\``)
+            .setDescription(`\`\`\`md\n<\nType a message you want to send to my owner!\n>\n\`\`\`\n\`\`\`md\nExample:\n<+report I'm having trouble with the calculation command. Can you help me?>\n\`\`\`\n\`\`\`md\n<Disclaimer: By sending this message, my owner will be able to get your User Tag and then send you a direct message to help you!>\n\`\`\`\n\`\`\`md\n# You can enter on the official server, if you want more help!\n\`\`\``)
             .addField("**Server Link:**", "https://discord.gg/QdqB5sR")
             .setFooter(`DisCalculus | Currently on ${bot.guilds.cache.size} servers!`)
 
