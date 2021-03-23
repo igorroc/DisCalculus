a6cdbe429e6702bfa597cce1d8b6404e9585f7d0
IgorRoc <igor_roc@hotmail.com.br>
2021-03-23 17:41:31 -0300

    [add]: Works better

--- commands/calculate.js
+++ commands/calculate.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-
+const mathjs = require("mathjs")
 const pi = '3.14159265359'
 
 
@@ -9,34 +9,31 @@ module.exports.run = async (bot, message, args) => {
     console.log(`\n■▶ [LOGS] ⇥ Usuário '${message.author.username}' usou o comando 'Calculate' no server '${message.guild.name}'`)
     log.send(`\\▶ [LOGS] ⇥ Usuário \` ${message.author.username} \` usou o comando \` Calculo \` no server \` ${message.guild.name} \`\n`)
 
-    let conta = args.toString().replace(/pi|π/gm, pi).replace(/÷/gm, '/').replace(/,/gm, "").replace(/×/gm, "*").replace(/ /gm, "")
-
-    if(!conta){
+    let conta = args.join(" ")
+    if(!conta || conta.search("parse","evaluate") != -1){
         console.log(`↳ ⚠️  Usuário '${message.author.username}' não enviou uma conta.`)
         log.send(`↳ \\⚠️  Usuário \` ${message.author.username} \` não enviou uma conta.`)
         return message.channel.send("`❌` Enter an calculation for me to perform\n> For more information type ` +help calculus `")
-    }
-    if(conta.search(/[a-z]|[A-Z]/gm) > -1){
-        console.log(`↳ ⚠️  Usuário '${message.author.username}' digitou caracteres: '${conta}'`)
-        log.send(`↳ \\⚠️  Usuário \` ${message.author.username} \` digitou caracteres: \` ${conta} \``)
-        return message.channel.send("`❌` I can't do advanced calculations yet.")
     }
 
 
     let resultado
     try {
-        if (conta.indexOf("^") > -1) {
-            var base = conta.slice(0, conta.indexOf("^"));
-            var exponent = conta.slice(conta.indexOf("^") + 1);
-            resultado = eval("Math.pow(" + base + "," + exponent + ")");
-        }else{
-            resultado = eval(conta)
-        }
+        resultado = mathjs.fraction(conta)
     } catch (error) {
-        console.log(`↳ ⚠️  Erro ao calcular '${conta}'`)
-        log.send(`↳ \\⚠️  Erro ao calcular \` ${conta} \``)
-        return message.channel.send("`❌` Error when calculating.")
+        try {
+            resultado = mathjs.evaluate(conta)
+        } catch (error) {
+            try {
+                resultado = mathjs.simplify(conta)
+            } catch (error) {
+                console.log(`↳ ⚠️  Erro ao calcular '${conta}'`)
+                log.send(`↳ \\⚠️  Erro ao calcular \` ${conta} \``)
+                return message.channel.send("`❌` Error when calculating.")
+            }
+        }
     }
+    
     
 
     message.channel.send(`\`\`\`\n${conta} = ${resultado}\n\`\`\``)
