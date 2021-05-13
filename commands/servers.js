ff7d84bfb415b13186f1a2fe0135966ddbd7d6e9
IgorRoc <igor_roc@hotmail.com.br>
2021-05-13 07:43:39 -0300

    [add]: Change serves quantity

--- commands/servers.js
+++ commands/servers.js
@@ -5,7 +5,7 @@ const config = require("../config.json")
 
 module.exports.run = async (bot, message, args) => {
     const log = bot.guilds.cache.get('725691740538929225').channels.cache.get('725691977311453214')
-
+    
     console.log(`\n■▶ [LOGS] ⇥ Usuário '${message.author.username}' usou o comando 'Servers' no server '${message.guild.name}'`)
     log.send(`\\▶ [LOGS] ⇥ Usuário \` ${message.author.username} \` usou o comando \` Servers \` no server \` ${message.guild.name} \`\n`)
 
@@ -24,6 +24,8 @@ module.exports.run = async (bot, message, args) => {
     
     await message.channel.send(SHembed);
 
+    let servers = bot.guilds.cache.get('725691740538929225').channels.cache.get('842346193787027496')
+    servers.setName(`🤖▏ Servers: ${bot.guilds.cache.size}`)
     
     console.log(`↳ ✅ Operação finalizada!`)
     log.send(`↳ \\✅ Operação finalizada!`)
